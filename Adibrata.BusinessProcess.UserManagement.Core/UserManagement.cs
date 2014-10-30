68d93bf39cd9e7f9070ca98855ae16eec6e5d511
tanayafredy <fredy.tube@gmail.com>
2014-10-30 10:50:56 +0700

    update dataaccess

---
+++ Adibrata.BusinessProcess.UserManagement.Core/UserManagement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adibrata.BusinessProcess.UserManagement.Entities;
+
+namespace Adibrata.BusinessProcess.UserManagement.Core
+{
+    public class UserManagement
+    {
+        public static void UserMangementAddEdit (UserManagement _ent)
+        {
+
+        }
+    }
+}
---
+++ Adibrata.FinanceLease.Windows/UserManagement/UserRegistrationAddEdit.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Adibrata.BusinessProcess.UserManagement.Entities;
+using Adibrata.Controller.UserManagement;
+using System.Data;
+namespace Adibrata.FinanceLease.Windows.UserManagement
+{
+    /// <summary>
+    /// Interaction logic for UserRegistrationAddEdit.xaml
+    /// </summary>
+    public partial class UserRegistrationAddEdit : Page
+    {
+        string currentUserName;
+        UserManagementController _obj = new UserManagementController();
+
+       
+        public UserRegistrationAddEdit(string username = "0")
+        {
+
+            
+            InitializeComponent();
+            dpExp.DisplayDateStart = DateTime.Now;
+            currentUserName = username;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            UserManagementEntities _ent = new UserManagementEntities { MethodName = "UserMangementAddEdit", ClassName = "Adibrata.BusinessProcess.UserManagement.Extend.UserManagement" };
+            _obj.UserManagement<string>(_ent);
+        }
+    }
+}

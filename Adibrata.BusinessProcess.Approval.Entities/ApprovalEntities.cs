cdf973439e26ca68507fc752754a85c909cea1af
Henry Sudarma <henrysudarma@yahoo.com>
2014-10-31 14:05:25 +0700

    Push All

---
+++ Adibrata.BusinessProcess.Approval.Entities/ApprovalEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adibrata.BusinessProcess.Entities.Base;
+
+namespace Adibrata.BusinessProcess.Approval.Entities
+{
+    [Serializable]
+    public class ApprovalEntities:EntitiesBase
+    {
+
+    }
+}

35036b72d0a2a15ab7a90f99df64b59a7504ec18
Henry Sudarma <henrysudarma@yahoo.com>
2014-10-31 15:40:39 +0700

    Commit

--- Adibrata.BusinessProcess.UserManagement.Entities/UserManagementEntities.cs
+++ Adibrata.BusinessProcess.UserManagement.Entities/UserManagementEntities.cs
@@ -11,13 +11,10 @@ namespace Adibrata.BusinessProcess.UserManagement.Entities
     [Serializable]
     public class UserManagementEntities : EntitiesBase
     {
-        public DataTable ReportTable { get; set; }
-        public byte[] ReportData { get; set; }
-        public int CurrentPage { get; set; }
-        public int PageSize { get { return 20; } }
-        public string WhereCond { get; set; }
-        public string SortBy { get; set; }
-        public int TotalRecord { get; set; }
-        public string ConnectionString { get; set; } // Connection string di set di ui
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int MaxWrong { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public int IsActive { get; set; }
     }
 }

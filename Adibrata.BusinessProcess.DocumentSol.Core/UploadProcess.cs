e08529b936f37895ed6950ea4214571192602054
Henry Sudarma <henrysudarma@yahoo.com>
2014-11-02 10:14:21 +0700

    Add New Project Docsol.core

---
+++ Adibrata.BusinessProcess.DocumentSol.Core/UploadProcess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adibrata.BusinessProcess.DocumentSol.Core
+{
+    public class UploadProcess
+    {
+    }
+}

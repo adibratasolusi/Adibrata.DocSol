8283113872497bf9fb3d2e0e58d5e33b1598ac7e
tanayafredy <fredy.tube@gmail.com>
2014-11-04 10:00:50 +0700

    docsol.windows

---
+++ Adibrata.DocumentSol.Windows/AgrmntUpload/AgrmntUploadProc.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Adibrata.DocumentSol.Windows.AgrmntUpload
+{
+    /// <summary>
+    /// Interaction logic for AgrmntUploadProc.xaml
+    /// </summary>
+    public partial class AgrmntUploadProc : Page
+    {
+        public AgrmntUploadProc()
+        {
+            InitializeComponent();
+        }
+    }
+}

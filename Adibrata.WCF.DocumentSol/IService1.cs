bf358b1344c5534e723cb9bfd94be7fb36e7499c
tanayafredy <fredy.tube@gmail.com>
2014-11-04 11:03:15 +0700

    add folder for wcf

---
+++ Adibrata.WCF.DocumentSol/IService1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Adibrata.WCF.DocumentSol
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        void UpdatePathDetails(PathDetails pathInfo);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class PathDetails
+    {
+        string agrmntNo = string.Empty;
+        string docType = string.Empty;
+        string fileName = string.Empty;
+        string ext = string.Empty;
+        Int64 pathId;
+        byte[] fileBinary;
+
+        [DataMember]
+        public string AgrmntNo
+        {
+            get { return agrmntNo; }
+            set { agrmntNo = value; }
+        }
+        [DataMember]
+        public string Ext
+        {
+            get { return ext; }
+            set { ext = value; }
+        }
+        [DataMember]
+        public string DocType
+        {
+            get { return docType; }
+            set { docType = value; }
+        }
+        [DataMember]
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+        [DataMember]
+        public byte[] FileBinary
+        {
+            get { return fileBinary; }
+            set { fileBinary = value; }
+        }
+        [DataMember]
+        public Int64 PathId
+        {
+            get { return pathId; }
+            set { pathId = value; }
+        }
+    }
+}
---
+++ Adibrata.WCF.DocumentSol/Service1.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+
+namespace Adibrata.WCF.DocumentSol
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        string server = ConfigurationManager.AppSettings["Server"];
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public void UpdatePathDetails(PathDetails pathInfo)
+        {
+
+
+            var webClient = new WebClient();
+            byte[] fileBytes = webClient.DownloadData(server + pathInfo.FileName + pathInfo.Ext);
+            string strMessage = string.Empty;
+            SqlConnection con = new SqlConnection(conString);
+            int result = 0;
+            try
+            {
+                SqlCommand command = new SqlCommand("spUpdatePath", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@file", SqlDbType.VarChar).Value = pathInfo.FileName;
+                command.Parameters.Add("@bin", SqlDbType.VarBinary).Value = fileBytes;
+                command.Parameters.Add("@ext", SqlDbType.VarChar).Value = pathInfo.Ext;
+                con.Open();
+                result = command.ExecuteNonQuery();
+                con.Close();
+
+                if (result == 1)
+                {
+
+                    //logging success here
+                }
+                else
+                {
+
+                    //logging error db here
+                }
+            }
+            catch (Exception ex)
+            {
+                //logging app here
+            }
+
+        }
+    }
+}

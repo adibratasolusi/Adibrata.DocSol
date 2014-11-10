c669fd5e38ce272b5f911184ca031ac038465148
Henry Sudarma <henrysudarma@yahoo.com>
2014-11-10 12:31:38 +0700

    Add new Method User Name Password Verification

--- Adibrata.BusinessProcess.UserManagement.Core/UserManagement.cs
+++ Adibrata.BusinessProcess.UserManagement.Core/UserManagement.cs
@@ -9,6 +9,7 @@ using Adibrata.Framework.DataAccess;
 using Adibrata.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using Adibrata.Framework.Security;
 
 namespace Adibrata.BusinessProcess.UserManagement.Core
 {
@@ -17,11 +18,11 @@ namespace Adibrata.BusinessProcess.UserManagement.Core
 
         static string Connectionstring = AppConfig.Config("ConnectionString");
 
-        public virtual void UserMangementAddEdit (UserManagementEntities _ent)
+        public virtual void UserMangementAddEdit(UserManagementEntities _ent)
         {
             SqlParameter[] sqlParams = new SqlParameter[4];
             sqlParams[0] = new SqlParameter("@UserName", SqlDbType.VarChar, 50);
-            sqlParams[0].Value = _ent.UserName; 
+            sqlParams[0].Value = _ent.UserName;
             sqlParams[1] = new SqlParameter("@Password", SqlDbType.VarChar, 300);
             sqlParams[1].Value = _ent.Password;
             sqlParams[2] = new SqlParameter("@MaxWrong", SqlDbType.Int);
@@ -33,5 +34,31 @@ namespace Adibrata.BusinessProcess.UserManagement.Core
             SqlHelper.ExecuteNonQuery(Connectionstring, CommandType.StoredProcedure, "spUserManagementAddEdit");
         }
 
+        public virtual Boolean UserNamePasswordVerification(UserManagementEntities _ent)
+        {
+            string _password;
+            Boolean _isvalid;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Select Password from MSUser With (nolok) where UserName = @UserName");
+
+            SqlParameter[] sqlParams = new SqlParameter[0];
+            sqlParams[0] = new SqlParameter("@UserName", SqlDbType.VarChar, 50);
+            sqlParams[0].Value = _ent.UserName;
+
+            _password = (string)SqlHelper.ExecuteScalar(Connectionstring, CommandType.Text, sb.ToString(), sqlParams);
+            
+            if (_password == Encryption.EncryptToSHA3(_ent.Password))
+            {
+                _isvalid = true;
+            }
+            else
+            {
+                _isvalid = false;
+            }
+            return _isvalid;
+
+        }
+
     }
 }
+

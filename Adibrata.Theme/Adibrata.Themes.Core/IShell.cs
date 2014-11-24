ed30ab87bf2ccddc2c07b70dbb8fe6f713253a2b
tanayafredy <fredy.tube@gmail.com>
2014-11-24 16:04:15 +0700

    Theme + Prototype

---
+++ Adibrata.Theme/Adibrata.Themes.Core/IShell.cs
@@ -0,0 +1,19 @@
+﻿using Adibrata.Themes.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adibrata.Themes.Prototype.WPF
+{
+    public interface IShell
+    {
+        bool IsLightVersion { get; }
+
+        void ExitApplication();
+
+        void ShowMessage(string message, string title);
+
+        IWindow CreateWindow(object userControl, string title);
+    }
+}
---
+++ Adibrata.Theme/Adibrata.Themes.Core/IWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adibrata.Themes.Core
+{
+    public interface IWindow
+    {
+        void Show();
+
+        void Close();
+
+        event EventHandler Closed;
+    }
+}

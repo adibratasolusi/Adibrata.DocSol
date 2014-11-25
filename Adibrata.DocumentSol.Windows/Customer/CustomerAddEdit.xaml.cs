3706d5f1d6039652830a73a44a69ebee975cb80c
Henry Sudarma <henrysudarma@yahoo.com>
2014-11-25 17:31:50 +0700

    Customer Add Edit

--- Adibrata.DocumentSol.Windows/Customer/CustomerAddEdit.xaml.cs
+++ Adibrata.DocumentSol.Windows/Customer/CustomerAddEdit.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Adibrata.Windows.UserController;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Adibrata.DocumentSol.Windows.Customer
         public CustomerAddEdit()
         {
             InitializeComponent();
+
+            this.DataContext = new MainVM(new Shell());
         }
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)

9ac6d166af3ecaca7a8fffd485a48f8de1be073d
Henry Sudarma <henrysudarma@yahoo.com>
2014-11-26 17:43:58 +0700

    Add new Class For User Registrasi Paging

--- Adibrata.BusinessProcess.Paging.Extend/Customer/CustomerRegistrasi.cs
+++ Adibrata.BusinessProcess.Paging.Extend/Customer/CustomerRegistrasi.cs
@@ -1,34 +1,42 @@
-﻿using Adibrata.BusinessProcess.Paging.Entities;
-using Adibrata.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adibrata.BusinessProcess.Paging.Entities;
 using Adibrata.Framework.DataAccess;
 using Adibrata.Framework.Logging;
-using System;
+using Adibrata.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Text;
 
-namespace Adibrata.BusinessProcess.Paging.Extend.UserManagement
+
+
+namespace Adibrata.BusinessProcess.Paging.Extend
 {
-   public class UserRegisterPaging: Adibrata.BusinessProcess.Paging.Core.UserManagement.UserRegisterPaging
+    public class CustomerRegistrasi
     {
-        static string Connectionstring = AppConfig.Config("ConnectionString");
-        public virtual DataTable UserRegister(PagingEntities _ent)
+        static string ConnectionString = AppConfig.Config("ConnectionString");
+
+        public virtual DataTable CustomerPaging (PagingEntities _ent)
         {
             DataTable _dt = new DataTable();
             StringBuilder sb = new StringBuilder();
             try
             {
-                sb.Append("spMsUserPaging");
+                sb.Append("spCustomerPaging");
                 SqlParameter[] sqlParams = new SqlParameter[4];
                 sqlParams[0] = new SqlParameter("@StartRecord", SqlDbType.Int);
                 sqlParams[0].Value = _ent.StartRecord;
-                sqlParams[1] = new SqlParameter("@EndEndRecord", SqlDbType.Int);
+                sqlParams[1] = new SqlParameter("@EndRecord", SqlDbType.Int);
                 sqlParams[1].Value = _ent.EndRecord;
+
                 sqlParams[2] = new SqlParameter("@wherecond", SqlDbType.VarChar, 500);
                 sqlParams[2].Value = _ent.WhereCond;
                 sqlParams[3] = new SqlParameter("@sortby", SqlDbType.VarChar, 500);
                 sqlParams[3].Value = _ent.SortBy;
-                //_dt = (DataTable)SqlHelper.ExecuteDataset(Connectionstring, CommandType.StoredProcedure, sb.ToString(), sqlParams).Tables[0];
+                _dt.Load(SqlHelper.ExecuteReader(ConnectionString, CommandType.StoredProcedure, sb.ToString(), sqlParams));
+                
             }
             catch (Exception _exp)
             {
@@ -47,6 +55,7 @@ namespace Adibrata.BusinessProcess.Paging.Extend.UserManagement
                 ErrorLog.WriteEventLog(_errent);
             }
             return _dt;
+            
         }
     }
 }

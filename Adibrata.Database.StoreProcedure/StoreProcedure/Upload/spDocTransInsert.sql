a5783e1fe423f37ec6fe55583e964e82b3d3942c
tanayafredy <fredy.tube@gmail.com>
2014-12-08 16:43:18 +0700

    image process + sp upload + uc scan

---
+++ Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spDocTransInsert.sql
@@ -0,0 +1,48 @@
+﻿
+-- =============================================
+-- Author:		Nurul Fredyani Tanaya
+-- Create date: 20141110
+-- Description:	insert ke tabel DocTrans
+-- =============================================
+ALTER PROCEDURE [dbo].[spDocTransInsert]
+	-- Add the parameters for the stored procedure here
+	@TransId bigint,
+	@docType varchar(50)
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	BEGIN TRAN A
+    -- Insert statements for procedure here
+	INSERT INTO DocTrans
+	(
+		TransID,
+		DocTypeCode,
+		UsrCrt,
+		DtmCrt,
+		UsrUpd,
+		DtmUpd
+
+	) 
+	OUTPUT inserted.Id 
+	VALUES
+	(
+		@TransId,
+		@docType,
+		'sa',
+		GETDATE(),
+		'sa',
+		GETDATE()
+	)
+		 IF (@@ERROR <> 0) BEGIN
+        ROLLBACK TRAN A
+        RETURN 1
+    END
+
+COMMIT TRAN A
+
+RETURN 0
+
+END
---
+++ Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spUpdatePath.sql
@@ -0,0 +1,59 @@
+﻿
+-- =============================================
+-- Author:		Nurul Fredyani Tanaya
+-- Create date: 29/10/2014
+-- Description:	SP Untuk update binary di table path, input berupa gambar
+-- =============================================
+ALTER PROCEDURE [dbo].[spUpdatePath]
+ 
+ 
+	@DocTransID BigInt,
+	@FileName varchar(8000),
+	@DateCreated datetime,
+	@SizeFileBytes numeric(20,0),
+	@Pixel varchar(100) = 0,
+	@ComputerName varchar(100) = 0,
+	@DPI varchar(100) = 0,
+	@FileBinary varbinary(max)
+
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	INSERT INTO DocTransBinary
+		(
+		DocTransID,
+		FileName,
+		DateCreated,
+		SizeFileBytes,
+		Pixel,
+		ComputerName,
+		DPI,
+		FileBinary,
+		UsrUpd,
+		DtmUpd,
+		UsrCrt,
+		DtmCrt
+		)
+	VALUES
+		(
+		@DocTransID,
+		@FileName,
+		@DateCreated,
+		@SizeFileBytes,
+		@Pixel,
+		@ComputerName,
+		@DPI,
+		@FileBinary,
+		'sa',
+		GETDATE(),
+		'sa',
+		GETDATE()
+		)
+
+	--select @file +'asdasd'
+    -- Insert statements for procedure here
+	--update PATH set FILE_NAME = @file+@ext, BINARY = @bin where PATH_ID = cast(@file as int)
+END

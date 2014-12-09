d4dd402d40bf588a4d21fd1acc52cc722273c87c
tanayafredy <fredy.tube@gmail.com>
2014-12-09 16:26:25 +0700

    commit

---
+++ Adibrata.Windows.UserController/ImageProcess/UCImageToPdf.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Adibrata.Windows.UserController.ImageProcess
+{
+    /// <summary>
+    /// Interaction logic for UCImageToPdf.xaml
+    /// </summary>
+    public partial class UCImageToPdf : UserControl
+    {
+        public UCImageToPdf()
+        {
+            InitializeComponent();
+        }
+    }
+}

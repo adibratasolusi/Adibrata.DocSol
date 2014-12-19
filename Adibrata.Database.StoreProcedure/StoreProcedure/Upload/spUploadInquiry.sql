b917259baef678232d36b6fbec94ae8dd6f52de2
Henry Sudarma <henrysudarma@yahoo.com>
2014-12-19 11:28:07 +0700

    Revise spUpload

--- Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spUploadInquiry.sql
+++ Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spUploadInquiry.sql
@@ -3,7 +3,7 @@
 -- Create date: 20141218
 -- Description:	Stored Procedures fo Upload Inquiry
 -- =============================================
-CREATE PROCEDURE spUploadInquiry
+ALTER PROCEDURE [dbo].[spUploadInquiry]
 	@StartRecord varchar(10), 
 	@EndRecord varchar(10), 
 	@WhereCond varchar(8000), 
@@ -18,9 +18,14 @@ If @SortBy = ''
 	Set @SortBy = ' TransID Asc '
 
 	Set @SqlStatement = 'Select * from 
-		(Select ROW_NUMBER() OVER (Order By ' + @SortBy + ') as number, TransID,DocTypeCode
-		From DocTrans With (nolock)  ' + @WhereCond  + ') Qry
+		(Select ROW_NUMBER() OVER (Order By ' + @SortBy + ') as number, DocTransCode, TransID, DocTypeCode, Proj.ProjCode, Proj.ProjName, Doctrans.DocTransStatus ,
+		Cust.CustCode, Cust.CustName
+		From DocTrans With (nolock) Inner Join 	Proj with (nolock) on Doctrans.Transid = Proj.ID 
+		inner join cust on Proj.CustID = cust.id
+		  ' 
+		+ @WhereCond  + ') Qry
 		where number between ' + @StartRecord  + ' and  ' + @EndRecord  
+		print @SqlStatement
 		exec (@SqlStatement)
 Set @TotalRecord =  @@ROWCOUNT
 RETURN 0

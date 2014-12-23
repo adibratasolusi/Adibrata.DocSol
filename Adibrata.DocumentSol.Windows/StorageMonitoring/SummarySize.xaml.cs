9384aee1e25f1b3561ccf19d60ce501b67a531ad
tanayafredy <fredy.tube@gmail.com>
2014-12-23 14:15:52 +0700

    filestorage page

---
+++ Adibrata.DocumentSol.Windows/StorageMonitoring/SummarySize.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Adibrata.BusinessProcess.Entities.Base;
+using Adibrata.Windows.UserController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Adibrata.DocumentSol.Windows.StorageMonitoring
+{
+    /// <summary>
+    /// Interaction logic for SummarySize.xaml
+    /// </summary>
+    public partial class SummarySize : Page
+    {
+        SessionEntities SessionProperty = new SessionEntities();
+        public SummarySize(SessionEntities _session)
+        {
+            InitializeComponent();
+            this.DataContext = new MainVM(new Shell());
+            SessionProperty = _session;
+        }
+    }
+}

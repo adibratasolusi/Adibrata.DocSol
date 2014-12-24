c20f548f6e8106621fd9b21a57feedb492eb4233
Henry Sudarma <henrysudarma@yahoo.,com>
2014-12-23 17:56:17 -0800

    Doc Trans Approval

--- Adibrata.BusinessProcess.DocumentSol.Extend/Approval/ApprovalProcess.cs
+++ Adibrata.BusinessProcess.DocumentSol.Extend/Approval/ApprovalProcess.cs
@@ -15,6 +15,8 @@ namespace Adibrata.BusinessProcess.DocumentSol.Extend
     {
           static string ConnectionString = AppConfig.Config("ConnectionString");
           string _levelapproval;
+
+          SqlTransaction _trans;
           public virtual string ApprovalPathRetrieve(DocSolEntities _ent)
           {
               DataTable _dt = new DataTable();
@@ -220,6 +222,62 @@ namespace Adibrata.BusinessProcess.DocumentSol.Extend
               }
               return _value;
 
+        }
+
+        public virtual void ApprovalScreenSave (DocSolEntities _ent)
+        {
+            SqlParameter[] sqlParams;
+            SqlConnection _conn = new SqlConnection(ConnectionString);
+            try
+            {
+                if (_conn.State == ConnectionState.Closed) { _conn.Open(); };
+                _trans = _conn.BeginTransaction();
+                //@DocTransApprID BigInt, 
+                //@LastUserAppr varchar(50), 
+                //@NextLevelAppr varchar(2), 
+                //@CurrentLevelAppr varchar(2), 
+                //@DocApprStat varchar(10), 
+                //@RequestTo varchar(50), 
+                //@ApprovalNotes Varchar(8000)
+                sqlParams = new SqlParameter[8];
+                sqlParams[0] = new SqlParameter("@DocTransApprID", SqlDbType.VarChar, 50);
+                sqlParams[0].Value = _ent.DocumentType;
+                sqlParams[1] = new SqlParameter("@LastUserAppr", SqlDbType.BigInt);
+                sqlParams[1].Value = _ent.UserLogin;
+                sqlParams[2] = new SqlParameter("@NextLevelAppr", SqlDbType.SmallDateTime);
+                sqlParams[2].Value = DateTime.Now;
+                sqlParams[3] = new SqlParameter("@CurrentLevelAppr", SqlDbType.VarChar, 50);
+                sqlParams[3].Value = _ent.UserLogin;
+                sqlParams[4] = new SqlParameter("@DocApprStat", SqlDbType.VarChar, 50);
+                sqlParams[4].Value = _ent.RequestTo;
+                sqlParams[5] = new SqlParameter("@RequestTo", SqlDbType.VarChar, 8000);
+                sqlParams[5].Value = _ent.ApprovalNotes;
+                sqlParams[6] = new SqlParameter("@ApprovalNotes", SqlDbType.VarChar, 50);
+                sqlParams[6].Value = _ent.UserLogin;
+                sqlParams[7] = new SqlParameter("@UsrUpd", SqlDbType.VarChar,50);
+                sqlParams[7].Value = _ent.UserLogin;
+                sqlParams[7] = new SqlParameter("@DtmUpd", SqlDbType.SmallDateTime);
+                sqlParams[7].Value = DateTime.Now;
+                SqlHelper.ExecuteNonQuery(_trans, CommandType.StoredProcedure, "spDocTransApprovalRequest", sqlParams);
+                _trans.Commit();
+            }
+            catch (Exception _exp)
+            {
+                _trans.Rollback();
+                ErrorLogEntities _errent = new ErrorLogEntities
+                {
+                    UserLogin = _ent.UserLogin,
+                    NameSpace = "Adibrata.BusinessProcess.DocumentSol.Extend",
+                    ClassName = "ApprovalProcess",
+                    FunctionName = "ApprovalRequestTo",
+                    ExceptionNumber = 1,
+                    EventSource = "DocContent",
+                    ExceptionObject = _exp,
+                    EventID = 200, // 80 Untuk Framework 
+                    ExceptionDescription = _exp.Message
+                };
+                ErrorLog.WriteEventLog(_errent);
+            }
         }
     }
 }

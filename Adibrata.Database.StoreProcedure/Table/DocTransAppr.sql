51356b56cf46c8212b6cd6bb37897a695ef7f621
Henry Sudarma <henrysudarma@yahoo.,com>
2014-12-23 17:19:40 -0800

    Doc Content Approval

--- Adibrata.Database.StoreProcedure/Table/DocTransAppr.sql
+++ Adibrata.Database.StoreProcedure/Table/DocTransAppr.sql
@@ -1,4 +1,4 @@
-﻿Drop TABLE [DocTransAppr]
+﻿--Drop TABLE [DocTransAppr]
 CREATE TABLE [dbo].[DocTransAppr]
 (
 	[ID] BIGINT PRIMARY KEY, 
@@ -7,8 +7,8 @@ CREATE TABLE [dbo].[DocTransAppr]
 	DocTransReqUser Varchar(50), 
 	DocTransReqDate smalldatetime null, 
 	DocTransApprCode Varchar(50), 
-	NextLevelAppr varchar(1), 
-	CurrentLevelAppr varchar(1), 
+	NextLevelAppr int, 
+	CurrentLevelAppr int, 
 	LastUserAppr Varchar(50),
 	DocApprStat varchar(50), 
 	DtmLastApproval smalldatetime,
@@ -19,7 +19,7 @@ CREATE TABLE [dbo].[DocTransAppr]
 	[DtmCrt] [smalldatetime] 
 ) On Approval
 
-
+Go 
 Create Index IX_DocContentAppr_DocTypeCode_CurrentLevelApp on DocTransAppr (DocTypeCode, CurrentLevelAppr) On IndexTbl
 Create Index IX_DocContentAppr_DocTypeCode_NextLevelAppr on DocTransAppr (DocTypeCode, NextLevelAppr) On IndexTbl
 

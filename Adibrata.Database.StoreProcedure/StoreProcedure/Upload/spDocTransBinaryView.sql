a34ba463aaccd8bfef8a79101cde2af31c6083b8
tanayafredy <fredy.tube@gmail.com>
2014-12-25 15:45:48 +0700

    doc trans log

--- Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spDocTransBinaryView.sql
+++ Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spDocTransBinaryView.sql
@@ -5,13 +5,15 @@
 -- =============================================
 CREATE PROCEDURE spDocTransBinaryView 
 	-- Add the parameters for the stored procedure here
-	@DocTransID bigint
+	@DocTransID bigint,
+	@UserName varchar(50)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
+	
+	exec spDocTransActivityInsert @username = @UserName, @DocTransId = @DocTransId,@description = 'Binary View'
 	select 
 	FileName,DateCreated,SizeFileBytes,Pixel,ComputerName,DPI,
 	CAST(CASE WHEN DPI <> '-' THEN FileBinary ELSE null END as varbinary(MAX)) AS FileBin

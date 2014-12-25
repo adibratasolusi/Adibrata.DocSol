a226f8899def77e2837463520dfc574df53aae23
EadibrataS <mimpianakindonesia@gmail.com>
2014-12-25 15:52:49 +0700

    Revisi File Storage Paging

--- Adibrata.DocumentSol.Windows/ImageProcess/WaterMark/WaterMarkDetail.xaml.cs
+++ Adibrata.DocumentSol.Windows/ImageProcess/WaterMark/WaterMarkDetail.xaml.cs
@@ -31,6 +31,7 @@ namespace Adibrata.DocumentSol.Windows.ImageProcess.WaterMark
                 InitializeComponent();
                 this.DataContext = new MainVM(new Shell());
                 SessionProperty = _session;
+                ucView.Session = SessionProperty;
                 ucView.DocTransId = Convert.ToInt64(SessionProperty.ReffKey);
 
             }

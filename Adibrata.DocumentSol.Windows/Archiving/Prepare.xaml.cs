0415c8a4f980af7f39b6a304151a921263c50cce
EadibrataS <mimpianakindonesia@gmail.com>
2014-12-25 19:08:36 +0700

    Archieving Detail

--- Adibrata.DocumentSol.Windows/Archiving/Prepare.xaml.cs
+++ Adibrata.DocumentSol.Windows/Archiving/Prepare.xaml.cs
@@ -60,6 +60,35 @@ namespace Adibrata.DocumentSol.Windows.Archiving
 
         private void btnDetail_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                int i = dgPaging.SelectedIndex;
+
+                DataGridHelper oDataGrid = new DataGridHelper();
+                oDataGrid.dtg = dgPaging;
+                DataGridCell cell = oDataGrid.GetCell(i, 2);
+                TextBlock ReffKey = oDataGrid.GetVisualChild<TextBlock>(cell); // pass the DataGridCell as a parameter to GetVisualChild
+                SessionProperty.IsEdit = true;
+                SessionProperty.ReffKey = ReffKey.Text;
+                RedirectPage redirect = new RedirectPage(this, "Archiving.PrepareDetail", SessionProperty);
+            }
+            catch (Exception _exp)
+            {
+                ErrorLogEntities _errent = new ErrorLogEntities
+                {
+                    UserLogin = SessionProperty.UserName,
+                    NameSpace = "Adibrata.DocumentSol.Windows.Archiving",
+                    ClassName = "UnlockImagePaging",
+                    FunctionName = "btnUnlock_Click",
+                    ExceptionNumber = 1,
+                    EventSource = "ImageUnlock",
+                    ExceptionObject = _exp,
+                    EventID = 200, // 1 Untuk Framework 
+                    ExceptionDescription = _exp.Message
+                };
+                ErrorLog.WriteEventLog(_errent);
+            }
+        
 
         }
 

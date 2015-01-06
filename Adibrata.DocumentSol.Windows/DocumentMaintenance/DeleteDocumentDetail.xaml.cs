63fb579df3bc6d794cce1dc49ba43891c84e50f0
EadibrataS <mimpianakindonesia@gmail.com>
2015-01-06 10:21:07 +0700

    rev

--- Adibrata.DocumentSol.Windows/DocumentMaintenance/DeleteDocumentDetail.xaml.cs
+++ Adibrata.DocumentSol.Windows/DocumentMaintenance/DeleteDocumentDetail.xaml.cs
@@ -1,25 +1,11 @@
 ﻿using Adibrata.BusinessProcess.DocumentSol.Entities;
 using Adibrata.BusinessProcess.Entities.Base;
-using Adibrata.BusinessProcess.UserManagement.Entities;
 using Adibrata.Controller;
-using Adibrata.Controller.UserManagement;
 using Adibrata.Framework.Logging;
 using Adibrata.Windows.UserController;
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Adibrata.DocumentSol.Windows.DocumentMaintenance
 {
--- Adibrata.DocumentSol.Windows/ImageProcess/Checkin/CheckinDetail.xaml.cs
+++ Adibrata.DocumentSol.Windows/ImageProcess/Checkin/CheckinDetail.xaml.cs
@@ -4,19 +4,8 @@ using Adibrata.Controller;
 using Adibrata.Framework.Logging;
 using Adibrata.Windows.UserController;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Adibrata.DocumentSol.Windows.ImageProcess.Checkin
 {
@@ -73,6 +62,12 @@ namespace Adibrata.DocumentSol.Windows.ImageProcess.Checkin
             MessageBox.Show("Check In Succes");
             RedirectPage redirect = new RedirectPage(this, "ImageProcess.Checkin.CheckinPaging", SessionProperty);
     
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+            RedirectPage redirect = new RedirectPage(this, "ImageProcess.Checkin.CheckinPaging", SessionProperty);
         }
     }
 }

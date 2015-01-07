c19b139faf3b4207e3e11915f6748255e55ba8ce
Henry Sudarma <henrysudarma@yahoo.,com>
2015-01-07 18:37:04 +0700

    Add total Record

---
+++ Adibrata.Database.StoreProcedure/StoreProcedure/Project/spProjectTotRec.sql
@@ -0,0 +1,14 @@
+﻿ALTER PROCEDURE [dbo].[spProjectTotRec]
+	@WhereCond varchar(8000), 
+	@SortBy Varchar(8000)
+AS
+Set NoCount On 
+Declare @SqlStatement varchar(8000)
+		Set @SqlStatement = 'Select * from 
+		(Select Count(1) as TotalRecord 
+		From Proj With (nolock) Left Join 
+		Cust with (nolock) on Proj.CustID = Cust.ID 
+		left Join CustCoy with (nolock) on Cust.ID = CustCoy.CustID ' + @WhereCond  + ') Qry '
+		
+		exec (@SqlStatement)
+RETURN 0

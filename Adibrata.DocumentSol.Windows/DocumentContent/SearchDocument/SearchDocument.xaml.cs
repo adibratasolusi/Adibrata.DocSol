9994f3e0d68f06b8b5fc518a7634e597396df2ff
Henry Sudarma <henrysudarma@yahoo.,com>
2015-01-07 15:32:45 +0700

    Upload REvisi

--- Adibrata.DocumentSol.Windows/DocumentContent/SearchDocument/SearchDocument.xaml.cs
+++ Adibrata.DocumentSol.Windows/DocumentContent/SearchDocument/SearchDocument.xaml.cs
@@ -96,6 +96,7 @@ namespace Adibrata.DocumentSol.Windows.DocumentContent
                 TextBlock ReffKey = oDataGrid.GetVisualChild<TextBlock>(cell); // pass the DataGridCell as a parameter to GetVisualChild
                 SessionProperty.IsEdit = true;
                 SessionProperty.ReffKey = ReffKey.Text;
+                SessionProperty.SourceForm = "DocumentContent.SearchDocument";
                 RedirectPage redirect = new RedirectPage(this, "UploadInquiry.UploadDetailInquiry", SessionProperty);
             }
             catch (Exception _exp)

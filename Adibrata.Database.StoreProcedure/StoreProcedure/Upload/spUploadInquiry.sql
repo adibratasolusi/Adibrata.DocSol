fdf392d9bb89453c0e14eb6ab52930881466561b
Henry Sudarma <henrysudarma@yahoo.,com>
2015-01-07 19:40:32 +0700

    Upload Inquiry

--- Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spUploadInquiry.sql
+++ Adibrata.Database.StoreProcedure/StoreProcedure/Upload/spUploadInquiry.sql
@@ -15,17 +15,20 @@ Declare @RecordNumber Numeric,
 Set NoCount On 
 Declare @TotalRecord int
 If @SortBy = '' 
-	Set @SortBy = ' TransID Asc '
+	Set @SortBy = ' DocTrans.TransID Asc '
 
 	Set @SqlStatement = 'Select * from 
-		(Select ROW_NUMBER() OVER (Order By ' + @SortBy + ') as number, DocTransCode, TransID, DocTypeCode, Proj.ProjCode, Proj.ProjName, Doctrans.DocTransStatus ,
+		(Select ROW_NUMBER() OVER (Order By ' + @SortBy + ') as number, DocTransCode, TransID, DocTrans.DocTypeCode, Proj.ProjCode, Proj.ProjName, Doctrans.DocTransStatus ,
 		Cust.CustCode, Cust.CustName
+		
 		From DocTrans With (nolock) Inner Join 	Proj with (nolock) on Doctrans.Transid = Proj.ID 
-		inner join cust on Proj.CustID = cust.id
+		inner join cust with (nolock) on Proj.CustID = cust.id
+		--Inner Join DocTransContent A with (nolock) on A.DocTransID = DocTrans.ID
+		--Inner Join DocTransBinary B with (nolock) on B.DocTransID = DocTrans.ID
 		  ' 
 		+ @WhereCond  + ') Qry
 		where number between ' + @StartRecord  + ' and  ' + @EndRecord  
-		print @SqlStatement
+		
 		exec (@SqlStatement)
 Set @TotalRecord =  @@ROWCOUNT
 RETURN 0

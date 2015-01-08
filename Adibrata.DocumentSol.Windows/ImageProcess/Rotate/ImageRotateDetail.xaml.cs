f0c78fed1e0f4c0bf7b8d4692c3dbaa3c66146e6
Henry Sudarma <henrysudarma@yahoo.,com>
2015-01-08 16:00:23 +0700

    revise tampilan

--- Adibrata.DocumentSol.Windows/ImageProcess/Rotate/ImageRotateDetail.xaml.cs
+++ Adibrata.DocumentSol.Windows/ImageProcess/Rotate/ImageRotateDetail.xaml.cs
@@ -4,20 +4,8 @@ using Adibrata.Controller;
 using Adibrata.Framework.Logging;
 using Adibrata.Windows.UserController;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Adibrata.DocumentSol.Windows.ImageProcess.Rotate
 {

9186af9c00594d463fdc8207c18e6660abd49030
tanayafredy <fredy.tube@gmail.com>
2015-01-13 10:08:40 +0700

    twain dll

---
+++ Adibrata.Windows.UserController/DocContent/UploadAgreement/SelectSourceForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Adibrata.Windows.UserController.DocContent.UploadAgreement
+{
+    partial class SelectSourceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceListBox = new System.Windows.Forms.ListBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sourceListBox
+            // 
+            this.sourceListBox.FormattingEnabled = true;
+            this.sourceListBox.Location = new System.Drawing.Point(13, 13);
+            this.sourceListBox.Name = "sourceListBox";
+            this.sourceListBox.Size = new System.Drawing.Size(120, 95);
+            this.sourceListBox.TabIndex = 0;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(159, 69);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(75, 23);
+            this.selectButton.TabIndex = 1;
+            this.selectButton.Text = "button1";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click_1);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(159, 99);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "button2";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // SelectSourceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 150);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.sourceListBox);
+            this.Name = "SelectSourceForm";
+            this.Text = "SelectSourceForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox sourceListBox;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
